0d2726c2920122c2d872df77df535d7d1f04a355
Bruno Gehlen <tousepc@gmail.com>
2020-06-19 16:31:59 -0300

    Initial commit

---
+++ school_system/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school_system {
+    class Program {
+        static void Main(string[] args) {
+            Human sissy = new Human("Sissy", "Wagner");
+            sissy.IntroduceMyself();
+
+
+            Console.ReadKey();
+            }
+        }
+    }
+}
